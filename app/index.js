99d9f754b8f1c85bc7f6e12b88934e1be0905b69
guyu <290506494@qq.com>
2020-06-09 01:45:07 +0800

    fix: 数据库连接抽取

--- app/index.js
+++ app/index.js
@@ -3,19 +3,11 @@ const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 const error = require('koa-json-error')
 const parameter = require('koa-parameter')
-const { connectionStr } = require('./config')
 const App = new Koa();
 const routering = require('./routes');
 
-const mongoose = require('mongoose')
-mongoose.set('useFindAndModify', false)
-mongoose.connect(connectionStr, { useNewUrlParser: true },
-  () => {
-    console.log('mongodb 连接成功')
-  }
-)
-mongoose.connection.on('error', console.error)
-
+require('./connect')
+ 
 // 生产环境错误stack信息隐藏
 App.use(error({
   // 格式化错误信息
